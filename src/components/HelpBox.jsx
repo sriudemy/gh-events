1f412f427434ca38d3815e5eaa398bb5c0eaf494
sriudemy <sriudemy2880@gmail.com>
2024-12-01 14:31:51 +0100

    Update HelpBox.jsx

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,6 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
+      <h4>Pull Request</h4>
       <h3>{title}</h3>
       <p>{text}</p>
     </article>
