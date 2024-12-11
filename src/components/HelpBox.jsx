78d32b66e81dbf7efaf8a2b253c65e1bde1d6073
srinath1 <srinath.erp@gmail.com>
2024-12-11 13:15:24 +0100

    d_1

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -6,7 +6,7 @@ function HelpBox({ title, text }) {
   return (
     <article className="help-box">
       <h1>New Image_Dev</h1>
-      <h1>New Image_Dev_Changed_workflow_1</h1>
+      <h1>New Image_Dev_Changed_workflow_1_1</h1>
       <h3>{title}</h3>
       <p>{text}</p>
     </article>
